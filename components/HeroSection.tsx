3f4a0f5726461a1ca756d3cdc2ef4006cfb60091
Roger Woolie <rogerwoolie@RogerW-MacBook-Air.local>
2025-08-09 22:06:59 +0800

    This Update Automatically From zsh Script.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -71,7 +71,7 @@ export function HeroSection() {
           <div className="relative">
             <div className="relative z-10">
               <ImageWithFallback
-                src="../../images/lsspv4.png"
+                src="~/Documents/Pers_CScience_2025/greenviro_figma/images/lsspv4.png"
                 alt="Environmental solutions and sustainability"
                 className="rounded-2xl shadow-2xl w-full h-auto"
               />
